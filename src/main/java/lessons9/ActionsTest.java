90bb382adce8cf7818760e8ad234a8f378b59fd7
katerina2995 <kfriman@ukr.net>
2021-02-23 21:24:29 +0200

    Actions Test new

--- src/main/java/lessons9/ActionsTest.java
+++ src/main/java/lessons9/ActionsTest.java
@@ -1,4 +1,36 @@
 package lessons9;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.IOException;
+import java.security.Key;
+import java.util.List;
+
 public class ActionsTest {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("webdriver.chrome.driver", "C:\\webdriver1\\chromedriver.exe");
+        WebDriver webDriver = new ChromeDriver();
+        String str = "C:\\Users\\Student\\IdeaProjects\\Selenium\\src\\main\\resources\\actions.html";
+
+        webDriver.get(str);
+        Actions actions = new Actions(webDriver);
+        try {
+            List<WebElement> li = webDriver.findElements(By.tagName("li"));
+            actions.click(li.get(1))
+                    .keyDown(Keys.CONTROL)
+                    .moveToElement(li.get(6))
+                    .pause(2000)
+                    .click()
+                    .perform();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+webDriver.quit();
+    }
 }
